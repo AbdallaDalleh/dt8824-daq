01f2e856c24e014b7d62e6c6912dd46e2fc734ff
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2023-08-14 10:59:03 +0300

    Added sequencer for the average current.

--- iocBoot/iocdt8824/st.cmd
+++ iocBoot/iocdt8824/st.cmd
@@ -9,10 +9,15 @@ dbLoadDatabase "dbd/dt8824.dbd"
 dt8824_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords("db/dt8824.db")
+dbLoadRecords("db/srs-sr570.db")
 dbLoadRecords("db/iocAdminSoft.db", "IOC=$(IOC_PREFIX)")
 
 drvAsynIPPortConfigure("DT8824", "10.3.1.87:5025", 0, 0, 1);
 DT8824Configure("DT", "DT8824", 12, 2000)
 
 iocInit
+
+seq current, "SYSTEM=D08-ES-IC0"
+seq current, "SYSTEM=D08-ES-IC1"
+seq current, "SYSTEM=D08-ES-IC2"
 
